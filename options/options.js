1b41a16bd5bdf65a11bd5126293bbca14985123c
Crowdbotics <team@crowdbotics.com>
2023-11-02 15:33:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "animal-counter-44295",
+  url: "https://animal-counter-44295.botics.co",
+  api: "https://animal-counter-44295.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
